d642116f491f12f08a1b0d62a49c0fcd64adf1a4
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-10-22 17:02:50 +0200

    Finito e Corretto

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+ mix.js('resources/js/app.js', 'public/js')
+     .sass('resources/sass/app.scss', 'public/css').options({processCssUrls: false});
 
 // Full API
 // mix.js(src, output);
